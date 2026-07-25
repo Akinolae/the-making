5477afbd5b4932215aaee1b2f5f63ec35e792774
Makinde Akinola <48059516+Akinolae@users.noreply.github.com>
2026-07-25 12:01:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tailwindcss(),
-  ],
+  base: "/",
+  plugins: [TanStackRouterVite(), react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
     },
