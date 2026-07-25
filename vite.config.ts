817766630fda022b1711fc098a6cf7db01d121a5
Makinde Akinola <48059516+Akinolae@users.noreply.github.com>
2026-07-25 12:04:39 +0100

    added new changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,4 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 export default defineConfig({
   base: "/",
   plugins: [TanStackRouterVite(), react(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
